fefa30e27d77c61190d83d79ea0375a15c844878
iamkunal18 <Anku_Vani12!>
2024-11-26 10:52:44 +0530

    Dependency Inversion Principle

--- solid_principles/dependency-inversion-principle.cpp
+++ solid_principles/dependency-inversion-principle.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct Employee
-{
-    virtual void work() = 0;
-};
-struct Developer : public Employee 
-{
-    void work() override;
-};
-struct Designer :public Employee
-{
-    void work() override;
-
+struct Employee {
+  virtual void work() = 0;
 };
 
-struct ResourceManager 
-{
-    std::vector<Employee> resources; // here Resouerce manager depends on employee not on developer or something, another kind of Employee will not make any change here
+struct Developer : public Employee {
+  void work() override;
 };
-int main()
-{
 
-    return;
-}
+struct Designer : public Employee {
+  void work() override;
+};
+
+struct ResourceManager {
+  std::vector<Employee>
+      resources; // here Resouerce manager depends on employee not on developer
+                 // or something, another kind of Employee will not make any
+                 // change here
+};
+
+int main() { return; }
